87aa213f5c4cd68aba4e2b30a2e3ec9c1d40c720
Layth <laythq@gmail.com>
2018-10-12 18:07:21 +0100

    Unit tests for CardList and all possible combinations of student and income

--- src/unit.js
+++ src/unit.js
@@ -1,11 +1,14 @@
 export class Card {
   constructor(name, apr, balanceTransferOfferDuration,
-              purchaseOfferDuration, creditAvailable) {
+              purchaseOfferDuration, creditAvailable,
+              isStudent, highIncome) {
     this.name = name;
     this.apr = apr;
     this.balanceTransferOfferDuration = balanceTransferOfferDuration;
     this.purchaseOfferDuration = purchaseOfferDuration;
     this.creditAvailable = creditAvailable;
+    this.isStudent = isStudent;
+    this.highIncome = highIncome;
   }
 }
 
@@ -13,4 +16,28 @@ export class CardList {
   constructor(cards) {
     this.cards = [...arguments]
   }
+
+  returnCards(isStudent, income) {
+      let firstFilter = this.studentScreen(isStudent)
+      return this.incomeScreen(firstFilter, income)
+  }
+
+  studentScreen(boolean) {
+    if (boolean) {
+      return this.cards
+    }
+    else {
+      return this.cards.filter(card => card.isStudent === false)
+    }
+  }
+
+  incomeScreen(cards, income) {
+    if (income > 16000) {
+      return cards
+    }
+    else {
+      return cards.filter(card => card.highIncome === false)
+    }
+  }
+
 }
--- src/unit.test.js
+++ src/unit.test.js
@@ -12,12 +12,30 @@ describe('Card#properties', () => {
 })
 
 describe('CardList', () => {
-  test('#cards returns an array of available cards', () => {
-    const card1 = new Card('Student Life', 18.9, 0, 6, 1200)
-    const card2 = new Card('Anywhere Card', 33.9, 0, 0, 300)
-    const card3 = new Card('Liquid Card', 33.9, 12, 6, 3000)
-    const cardList = new CardList(card1, card2, card3)
+  const card1 = new Card('Student Life', 18.9, 0, 6, 1200, true, false)
+  const card2 = new Card('Anywhere Card', 33.9, 0, 0, 300, false, false)
+  const card3 = new Card('Liquid Card', 33.9, 12, 6, 3000, false, true)
+  const cardList = new CardList(card1, card2, card3)
+
+  test('#cards property is an array of available cards', () => {
     expect(cardList.cards).toEqual([card1, card2, card3])
     expect(cardList.cards.length).toEqual(3)
+  })
+
+  test('#returnCards returns all three if student with income > 16000', () => {
+    expect(cardList.returnCards(true, 20000)).toEqual([card1, card2, card3])
+  })
+
+  test('#returnCards returns Student Life and Anywhere if student with income < 16000', () => {
+    expect(cardList.returnCards(true, 15000)).toEqual([card1, card2])
+  })
+
+  test('#returnCards returns Liquid Card and Anywhere if not student with income < 16000', () => {
+    expect(cardList.returnCards(false, 12000)).toEqual([card2])
+  })
+
+
+  test('#returnCards returns Anywhere and Liquid is not student with income > 16000', () => {
+    expect(cardList.returnCards(false, 20000)).toEqual([card2, card3])
   })
 })
